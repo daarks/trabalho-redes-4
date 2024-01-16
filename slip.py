27fe06e122a8eadf638ef4667d7192710a704a0a
daarks <105820474+daarks@users.noreply.github.com>
2024-01-16 16:28:31 -0300

    1a versao

--- slip.py
+++ slip.py
@@ -2,15 +2,6 @@ class CamadaEnlace:
     ignore_checksum = False
 
     def __init__(self, linhas_seriais):
-        """
-        Inicia uma camada de enlace com um ou mais enlaces, cada um conectado
-        a uma linha serial distinta. O argumento linhas_seriais é um dicionário
-        no formato {ip_outra_ponta: linha_serial}. O ip_outra_ponta é o IP do
-        host ou roteador que se encontra na outra ponta do enlace, escrito como
-        uma string no formato 'x.y.z.w'. A linha_serial é um objeto da classe
-        PTY (vide camadafisica.py) ou de outra classe que implemente os métodos
-        registrar_recebedor e enviar.
-        """
         self.enlaces = {}
         self.callback = None
         # Constrói um Enlace para cada linha serial
@@ -43,6 +34,7 @@ class Enlace:
     def __init__(self, linha_serial):
         self.linha_serial = linha_serial
         self.linha_serial.registrar_recebedor(self.__raw_recv)
+        self.buffer = b''
 
     def registrar_recebedor(self, callback):
         self.callback = callback
@@ -51,6 +43,7 @@ class Enlace:
         # TODO: Preencha aqui com o código para enviar o datagrama pela linha
         # serial, fazendo corretamente a delimitação de quadros e o escape de
         # sequências especiais, de acordo com o protocolo CamadaEnlace (RFC 1055).
+        self.linha_serial.enviar(b"\xc0" + self.escapar_datagrama(datagrama) + b"\xc0")
         pass
 
     def __raw_recv(self, dados):
@@ -61,4 +54,51 @@ class Enlace:
         # vir quebrado de várias formas diferentes - por exemplo, podem vir
         # apenas pedaços de um quadro, ou um pedaço de quadro seguido de um
         # pedaço de outro, ou vários quadros de uma vez só.
+
+        # Atualizamos os dados com os dados já guardados no buffer
+        dados = self.buffer + dados
+
+        # Dividimos possíveis datagramas misturados pelo separados
+        dados = dados.split(b'\xc0')
+
+        # Guardamos o resíduo no buffer (último elemento do array)
+        self.buffer = dados[-1]
+
+        # Enviamos todos os datagramas completos (menos o último) à camada superior
+        for datagrama in dados[:-1]:
+            # Não enviamos datagramas vazios
+            if datagrama != b"":
+                try:
+                    self.callback(self.traduzir_datagrama(datagrama))
+                except:
+                    # Ignoramos exceções
+                    import traceback
+
+                    traceback.print_exc()
+
+                finally:
+                    # Limpamos possíveis pedaços de datagramas
+                    dados = b''
+
+
         pass
+
+    def escapar_datagrama(self, datagrama):
+        
+        # Primeiro escapamos o byte 0xDB que serve para escapar 0xC0
+        datagrama_tratado_1 = datagrama.replace(b"\xdb", b"\xdb\xdd")
+
+        # Depois escapamos o byte 0xC0 dentro do datagrama para não haver ambiguidade
+        datagrama_tratado_final = datagrama_tratado_1.replace(b"\xc0", b"\xdb\xdc")
+
+        return datagrama_tratado_final
+    
+    def traduzir_datagrama(self, datagrama):
+
+        # Primeiro extraímos o byte especial 0xC0 escapado
+        datagrama_traduzido_1 = datagrama.replace(b"\xdb\xdc", b"\xc0")
+
+        # Depois extraímos o byte 0xDB escapado
+        datagrama_traduzido_final = datagrama_traduzido_1.replace(b"\xdb\xdd", b"\xdb")
+
+        return datagrama_traduzido_final
